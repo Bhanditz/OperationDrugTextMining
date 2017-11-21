f5a6136375b15e4b4cb49a65d518f58b8fa6e063
Luis Franco <luisfranco@mail.usf.edu>
2017-11-21 18:51:09 -0500

    Add files via upload

--- AutomatedDrug-SideEffect.R
+++ AutomatedDrug-SideEffect.R
@@ -7,7 +7,6 @@
 library(tm)
 library(gtools)
 library(stringr)
-library(wordcloud)
 library(SnowballC)
 library(stringr)
 library(rvest)
@@ -18,11 +17,14 @@ library(tidytext)
 library(dplyr)
 library(XML)
 library(xml2)
-require(sqldf)
+library(sqldf)
 
 pg_alive = TRUE
 user_url <- readline(prompt="Please enter the Yahoo! Answers search url:\n")
+selection <- readline(prompt="Select Output,SideEffectsFreq, DrugFreq or both:\n")
 
+firstpglist = NULL
+allpglist = NULL
 page <- read_html(user_url)
 firstpglist<- page %>% html_nodes(".fz-m") %>% html_attr('href')
 firstpglist <- firstpglist[-length(firstpglist)]
@@ -48,17 +50,23 @@ while (pg_alive){
 
 titleV = NULL
 realV = NULL
-for (i in 1:(length(allpglist)))
+
+if (length(allpglist) >= 1000)
 {
-  QuestPage <- read_html(allpglist[i])
-  titleV[i] <- QuestPage %>% html_nodes("title") %>% html_text("title")
-  realV[i] <- QuestPage %>% html_node(".ya-q-full-text") %>% html_text("text")
+  for (i in 1:999)
+  {
+    QuestPage <- read_html(allpglist[i])
+    titleV[i] <- QuestPage %>% html_nodes("title") %>% html_text("title")
+    realV[i] <- QuestPage %>% html_node(".ya-q-full-text") %>% html_text("text")
+  }
+}else{
+  for (i in 1:length(allpglist))
+  {
+    QuestPage <- read_html(allpglist[i])
+    titleV[i] <- QuestPage %>% html_nodes("title") %>% html_text("title")
+    realV[i] <- QuestPage %>% html_node(".ya-q-full-text") %>% html_text("text")
+  }
 }
-
-#Read in the question subject and full question
-#titleV <- readLines("C:/Users/Luisman/Desktop/USF FOLDERS/Fall 2017/CIS 4900 Independent Study in CS/DrugTextAnalysisFiles/Questions.txt")  
-#realV <- readLines("C:/Users/Luisman/Desktop/NewTest.txt")  
-
 #The full question
 QuestionV = NULL
 QuestionV = paste(titleV,realV,sep = " | ")
@@ -69,6 +77,13 @@ for (i in 1:length(QuestionV))
 
 QuestionV <- gsub("\\n","",QuestionV)
 QuestionV <- gsub("\\r","",QuestionV)
+
+QuestionWithUrl = NULL
+for (i in 1:length(QuestionV))
+{
+  QuestionWithUrl=rbind(QuestionWithUrl,c(i,QuestionV[i],allpglist[i]))
+}
+write.csv(QuestionWithUrl,file="QuestionWithUrl.csv")
 
 #Reads in possible sideeffects
 sideeffectsV <- readLines("C:/Users/Luisman/Desktop/BigDataSideEffects.txt")                                 
@@ -105,74 +120,61 @@ drugsV=drugsV[-RemoveDrug2]
 RemoveDrug3=which(drugsV=="water")
 drugsV=drugsV[-RemoveDrug3]
 
-
-#Getting the matching sideffects
-SideEffectList = NULL
-for (i in 1:length(QuestionV))
+if ((identical(selection,"SideEffectsFreq")) || (identical(selection,"both")))
 {
-  
-  for (j in 1:length(sideeffectsV))
+  #Getting the matching sideffects
+  SideEffectList = NULL
+  for (i in 1:length(QuestionV))
   {
-    id=grepl(sideeffectsV[j],QuestionV[i],fixed = TRUE)
-    if (id)
+  
+    for (j in 1:length(sideeffectsV))
     {
-      tmp<-c(i,sideeffectsV[j])
-      SideEffectList=rbind(SideEffectList,tmp)
+      id=grepl(sideeffectsV[j],QuestionV[i],fixed = TRUE)
+      if (id)
+      {
+        tmp<-c(i,sideeffectsV[j])
+        SideEffectList=rbind(SideEffectList,tmp)
+      }
     }
   }
+  write.csv(SideEffectList,file="SideEffectList.csv")
+  mydata = read.csv("SideEffectList.csv") 
+  CleanData <- subset(mydata, select = -c(X))
+  names(CleanData)[names(CleanData) == 'V1'] <- 'Question'
+  names(CleanData)[names(CleanData) == 'V2'] <- 'SideEffect'
+  SideEffectFreq <- sqldf("select SideEffect,Count(*)as Frequency from CleanData group by SideEffect order by Frequency desc")
+  TopTenSideEffects <- sqldf("select * from SideEffectFreq limit 10")
+  barplot(TopTenSideEffects$Frequency, main = "Top Ten Side Effects",ylab= "Frequency",names.arg=TopTenSideEffects$SideEffect,col="darkblue",las=2)
+  sink("SideEffectFreq.txt")
+  print(SideEffectFreq)
+  sink()
+  dev.copy(png,"TopTenSideEffectsChart.jpg")
+  dev.off()
 }
-
-#Getting the matching drugs
-DrugList = NULL
-for (i in 1:length(QuestionV))
+if ((identical(selection,"DrugFreq")) || (identical(selection,"both")))
 {
-  
-  for (j in 1:length(drugsV))
+  #Getting the matching drugs
+  DrugList = NULL
+  for (i in 1:length(QuestionV))
   {
-    id=grepl(drugsV[j],QuestionV[i],fixed = TRUE)
-    if (id)
+  
+    for (j in 1:length(drugsV))
     {
-      tmp<-c(i,drugsV[j])
-      DrugList=rbind(DrugList,tmp)
+      id=grepl(drugsV[j],QuestionV[i],fixed = TRUE)
+      if (id)
+      {
+        tmp<-c(i,drugsV[j])
+        DrugList=rbind(DrugList,tmp)
+      }
     }
   }
+  write.csv(DrugList,file="DrugList.csv")
+  mydata2 = read.csv("DrugList.csv")
+  CleanData2 <- subset(mydata2, select = -c(X))
+  names(CleanData2)[names(CleanData2) == 'V1'] <- 'Question'
+  names(CleanData2)[names(CleanData2) == 'V2'] <- 'Drug'
+  DrugFreq <- sqldf("select Drug,Count(*)as Frequency from CleanData2 group by Drug order by Frequency desc")
+  sink("DrugFreq.txt")
+  print(DrugFreq)
+  sink()
 }
-
-#Create csv files
-write.csv(SideEffectList,file="SideEffectList.csv")
-write.csv(DrugList,file="DrugList.csv")
-#Convert vectors to lists
-mydata = read.csv("SideEffectList.csv")  
-mydata2 = read.csv("DrugList.csv")
-
-#Remove unnecessary rows
-CleanData <- subset(mydata, select = -c(X))
-CleanData2 <- subset(mydata2, select = -c(X))
-
-#Rename columns in lists
-names(CleanData)[names(CleanData) == 'V1'] <- 'Question'
-names(CleanData)[names(CleanData) == 'V2'] <- 'SideEffect'
-names(CleanData2)[names(CleanData2) == 'V1'] <- 'Question'
-names(CleanData2)[names(CleanData2) == 'V2'] <- 'Drug'
-
-SideEffectFreq <- sqldf("select SideEffect,Count(*)as Frequency from CleanData group by SideEffect order by Frequency desc")
-
-DrugFreq <- sqldf("select Drug,Count(*)as Frequency from CleanData2 group by Drug order by Frequency desc")
-
-TopTenSideEffects <- sqldf("select * from SideEffectFreq limit 10")
-
-
-barplot(TopTenSideEffects$Frequency, main = "Top Ten Side Effects",ylab= "Frequency",names.arg=TopTenSideEffects$SideEffect,col="darkblue",las=2)
-
-
-sink("SideEffectFreq.txt")
-print(SideEffectFreq)
-sink()
-
-sink("DrugFreq.txt")
-print(DrugFreq)
-sink()
-
-dev.copy(png,"TopTenSideEffectsChart.jpg")
-dev.off()
-
