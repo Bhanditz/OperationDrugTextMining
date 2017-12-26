996a811053dd31c1d0885d50fdb892a512c68cfb
Luis Franco <luisfranco@mail.usf.edu>
2017-12-26 16:53:47 -0500

    Add files via upload

--- AutomatedDrugSideEffectIdentifierWithSorting.R
+++ AutomatedDrugSideEffectIdentifierWithSorting.R
@@ -1,11 +1,12 @@
 #*************************************************
-#AutomatedDrug-SideEffectIdentifier              *
+#AutomatedDrug-SideEffectIdentifierwithML        *
 #Authors: Luis Franco, Delroy Fong               *
 #Supervisers: Dr. Feng Cheng, Dr. Yicheng Tu     *
 #*************************************************
 
 library(tm)
 library(gtools)
+library(stringi)
 library(stringr)
 library(SnowballC)
 library(stringr)
@@ -18,8 +19,11 @@ library(dplyr)
 library(XML)
 library(xml2)
 library(sqldf)
-library(wordcloud)
+library(caret)
+library(installr)
 
+
+#R Crawling for Data
 pg_alive = TRUE
 user_url <- readline(prompt="Please enter the Yahoo! Answers search url:\n")
 selection <- readline(prompt="Select Output,SideEffectsFreq, DrugFreq or both:\n")
@@ -48,11 +52,12 @@ while (pg_alive){
   #checks if next button = 0, if true then no more next button left
   if (length(nextpg_url) == 0){pg_alive=FALSE}
 } 
-
+#Get only unique URLs and define title and real(question)
 allpglist <- unique(allpglist)
 titleV = NULL
 realV = NULL
 
+#Assure that yahoo will not timeout
 if (length(allpglist) >= 1000)
 {
   for (i in 1:999)
@@ -95,9 +100,84 @@ QuestionV <- gsub("\\r","",QuestionV)
 QuestionWithUrl = NULL
 for (i in 1:length(QuestionV))
 {
-  QuestionWithUrl=rbind(QuestionWithUrl,c(i,QuestionV[i],allpglist[i],"P"))
+  QuestionWithUrl=rbind(QuestionWithUrl,c(i,QuestionV[i],allpglist[i]))
 }
 write.csv(QuestionWithUrl,file="QuestionWithUrl.csv")
+
+#Define test dataframe with machine learning criteria
+Questdata = read.csv("QuestionWithUrl.csv") 
+Questdata <- subset(Questdata, select = -c(X))
+names(Questdata)[names(Questdata) == 'V1'] <- 'QuestionNumber'
+names(Questdata)[names(Questdata) == 'V2'] <- 'Question'
+names(Questdata)[names(Questdata) == 'V3'] <- 'Url'
+
+
+QuestionMLTest <- cbind(Questdata,NewColumn = 1)
+names(QuestionMLTest)[names(QuestionMLTest) == 'NewColumn'] <- 'Q/N'
+
+QuestionMLTest <- cbind(QuestionMLTest, Qlength = 1)
+QuestionMLTest <- cbind(QuestionMLTest, AveWordLen = 1)
+QuestionMLTest <- cbind(QuestionMLTest, my = 0)
+QuestionMLTest <- cbind(QuestionMLTest, I = 0)
+QuestionMLTest <- cbind(QuestionMLTest, take_verb = 0)
+QuestionMLTest <- cbind(QuestionMLTest, pill_words = 0)
+QuestionMLTest <- cbind(QuestionMLTest, feel_verb = 0)
+QuestionMLTest$QuestionNumber <- NULL
+QuestionMLTest$Url <- NULL
+
+#Getting the data for testing
+for (i in rownames(QuestionMLTest))
+{
+  #Test Q/n
+  if(grepl("no-question",QuestionMLTest[i,"Question"],fixed = TRUE))
+    QuestionMLTest[i,"Q/N"] <- 0
+  
+  #Test Qlength and AveWordLen
+  x <- QuestionMLTest[i,"Question"]
+  x <- gsub("[^0-9A-Za-z///']"," ",x,ignore.case = TRUE)
+  x <- gsub("yahoo answers|no question", "",x)
+  QuestionMLTest[i,"Qlength"] <- str_count(x,'\\w+')
+  words <- strsplit(x, " ")
+  j = 1
+  word_length = NULL
+  while (!(is.na(words[[1]][j])))
+  {
+    word_length <- rbind(word_length,nchar(words[[1]][j]))
+    j = j + 1
+  }
+  word_length <- word_length[-which(word_length == 0)]
+  QuestionMLTest[i,"AveWordLen"] <- round(mean(word_length),2)
+  
+  QuestCorp <- Corpus(VectorSource(QuestionMLTest[i,"Question"]))
+  QuestCorp <- tm_map(QuestCorp, stripWhitespace)
+  QuestCorp <- tm_map(QuestCorp, removeNumbers)
+  QuestCorp <- tm_map(QuestCorp, removePunctuation)
+  ndtm <- DocumentTermMatrix(QuestCorp,control=list(wordLengths=c(1,Inf)))
+  nfreq <- colSums(as.matrix(ndtm))
+  CorpWordList <- c("my","i","taking","take","taken","took","dose","pill","mg"
+                    ,"feeling","feel","felt")
+  
+  for (k in 1:length(CorpWordList)) 
+    if (is.na(nfreq[CorpWordList[k]]))
+      nfreq[CorpWordList[k]] <- 0
+  
+  QuestionMLTest[i,"my"] <- nfreq["my"] 
+  QuestionMLTest[i,"I"] <- nfreq["i"]
+  QuestionMLTest[i,"take_verb"] <- nfreq["taking"] + nfreq["take"] + nfreq["taken"] + nfreq["took"]
+  QuestionMLTest[i,"pill_words"] <- nfreq["dose"] + nfreq["pill"] + nfreq["mg"]
+  QuestionMLTest[i,"feel_verb"] <- nfreq["feeling"] + nfreq["feel"] + nfreq["felt"]
+  
+}
+
+#Analyzing Test Data to get patient Data
+TestData <- as.data.frame(QuestionMLTable1)
+TestData$Question <- NULL
+TestDataCat <- predict(QFit, newdata = TestData)
+
+TestData <- cbind(TestData,Classification = TestDataCat)
+TestData <- cbind(TestData,Question = QuestionV)
+PatientQuestions <- sqldf("select Question from TestData where Classification = 1")
+
 
 #Reads in possible sideeffects
 sideeffectsV <- readLines("C:/Users/Luisman/Desktop/USF FOLDERS/Fall 2017/CIS 4900 Independent Study in CS/DrugTextAnalysisFiles/BigDataSideEffects.txt")                                 
@@ -119,32 +199,29 @@ for (i in 1:length(sideeffectsV))
   sideeffectsV[i] <- sapply(sideeffectsV[i],tolower)
 
 #Remove unnecesarry entries in Sideeffects Vector
-RemoveEffect1=which(sideeffectsV=="tic")
-sideeffectsV=sideeffectsV[-RemoveEffect1]
+sideeffectsV <- sideeffectsV[-which(sideeffectsV=="tic")]
 
 #Lower Case Drugs Vector
 for (i in 1:length(drugsV))
   drugsV[i] <- sapply(drugsV[i],tolower)
 
 #Removes unnecesarry entries in Drugs Vector
-RemoveDrug1=which(drugsV=="na")
-drugsV=drugsV[-RemoveDrug1]
-RemoveDrug2=which(drugsV=="")
-drugsV=drugsV[-RemoveDrug2]
-RemoveDrug3=which(drugsV=="water")
-drugsV=drugsV[-RemoveDrug3]
+
+drugsV <- drugsV[-which(drugsV=="na")]
+drugsV <- drugsV[-which(drugsV=="")]
+drugsV <- drugsV[-which(drugsV=="water")]
+
 
 if ((identical(selection,"SideEffectsFreq")) || (identical(selection,"both")))
 {
   #Getting the matching sideffects
   SideEffectList = NULL
-  for (i in 1:length(QuestionV))
+  for (i in 1:length(PatientQuestions[,]))
   {
-  
+    
     for (j in 1:length(sideeffectsV))
     {
-      id=grepl(sideeffectsV[j],QuestionV[i],fixed = TRUE)
-      if (id)
+      if (grepl(sideeffectsV[j],PatientQuestions[i,],fixed = TRUE))
       {
         tmp<-c(i,sideeffectsV[j])
         SideEffectList=rbind(SideEffectList,tmp)
@@ -169,13 +246,11 @@ if ((identical(selection,"DrugFreq")) || (identical(selection,"both")))
 {
   #Getting the matching drugs
   DrugList = NULL
-  for (i in 1:length(QuestionV))
+  for (i in 1:length(PatientQuestions[,]))
   {
-  
     for (j in 1:length(drugsV))
     {
-      id=grepl(drugsV[j],QuestionV[i],fixed = TRUE)
-      if (id)
+      if (grepl(drugsV[j],PatientQuestions[i,],fixed = TRUE))
       {
         tmp<-c(i,drugsV[j])
         DrugList=rbind(DrugList,tmp)
